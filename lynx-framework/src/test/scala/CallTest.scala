294971408874136a81c07993c924cab9b9af3d12
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2021-01-13 18:21:54 +0800

    wrong call()

--- lynx-framework/src/test/scala/CallTest.scala
+++ lynx-framework/src/test/scala/CallTest.scala
@@ -1,4 +1,5 @@
-import org.grapheco.lynx.{LynxResult, LynxValue}
+import org.grapheco.lynx.{LynxResult, LynxValue, ParsingException, UnknownProcedureException, WrongNumberOfArgumentsException}
+import org.junit.function.ThrowingRunnable
 import org.junit.{Assert, Test}
 
 class CallTest extends TestBase {
@@ -11,5 +12,20 @@ class CallTest extends TestBase {
     Assert.assertEquals(Map("name" -> LynxValue("bluejoe")), rs.records().toSeq.apply(0))
     Assert.assertEquals(Map("name" -> LynxValue("lzx")), rs.records().toSeq.apply(1))
     Assert.assertEquals(Map("name" -> LynxValue("airzihao")), rs.records().toSeq.apply(2))
+  }
+
+  @Test
+  def testWrongCall(): Unit = {
+    Assert.assertThrows(classOf[UnknownProcedureException], new ThrowingRunnable() {
+      override def run(): Unit = {
+        runOnDemoGraph("call test.nonexisting()")
+      }
+    })
+
+    Assert.assertThrows(classOf[WrongNumberOfArgumentsException], new ThrowingRunnable() {
+      override def run(): Unit = {
+        runOnDemoGraph("call test.authors(2)")
+      }
+    })
   }
 }
