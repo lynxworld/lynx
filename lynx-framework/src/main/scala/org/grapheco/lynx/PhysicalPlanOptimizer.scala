e33fd88e8dfe016a99f4b4dcabd73131ac968192
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2021-01-29 22:11:35 +0800

    optimizer

--- lynx-framework/src/main/scala/org/grapheco/lynx/PhysicalPlanOptimizer.scala
+++ lynx-framework/src/main/scala/org/grapheco/lynx/PhysicalPlanOptimizer.scala
@@ -2,85 +2,43 @@ package org.grapheco.lynx
 
 import org.opencypher.v9_0.ast.{AliasedReturnItem, ReturnItemsDef}
 
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
-
 trait PhysicalPlanOptimizer {
   def optimize(plan: PPTNode): PPTNode
 }
 
-trait PPTNodeContext {
-  def replaceWith(orginal: PPTNode, newNode: PPTNode): Unit
+trait PhysicalPlanOptimizerRule {
+  def apply(plan: PPTNode): PPTNode
 
-  val downStream: Option[PPTNode]
+  def optimizeBottomUp(node: PPTNode, ops: PartialFunction[PPTNode, PPTNode]*): PPTNode = {
+    val childrenOptimized1 = node.withChildren(node.children.map(x => optimizeBottomUp(x, ops: _*)))
+    ops.foldLeft(childrenOptimized1) {
+      (optimized, op) =>
+        op.lift(optimized).getOrElse(optimized)
+    }
+  }
 }
 
-class AbstractPhysicalPlanOptimizer extends PhysicalPlanOptimizer {
-  private val rules = ArrayBuffer[(PPTNode, PPTNodeContext) => Option[PPTNode]]()
+class PhysicalPlanOptimizerImpl extends PhysicalPlanOptimizer {
+  val rules = Seq[PhysicalPlanOptimizerRule](
+    RemoveNullProject,
+  )
 
   def optimize(plan: PPTNode): PPTNode = {
-    //builds nodes map
-    val parents = mutable.Map[PPTNode, Option[PPTNode]]()
-    val transformed = mutable.Map[PPTNode, PPTNode]()
-    buildTopDown(plan, None, parents, transformed)
-
-    //do optimize
-    rules.foreach(rule =>
-      transformed.values.foreach(node => {
-        val ret = rule(node, new PPTNodeContext() {
-          //TODO: replace a->b, b->c
-          override val downStream: Option[PPTNode] = parents(node)
-
-          override def replaceWith(orginal: PPTNode, newNode: PPTNode): Unit = {
-            transformed += orginal -> newNode
-          }
-        })
-
-        ret match {
-          case None =>
-            transformed -= node
-
-          case Some(node2) if node2 != node =>
-            transformed += node -> node2
-
-          case _ =>
-        }
-      }
-      )
-    )
-
-    transformed(plan)
+    rules.foldLeft(plan)((optimized, rule) => rule.apply(optimized))
   }
-
-  private def buildTopDown(node: PPTNode, parent: Option[PPTNode], parents: mutable.Map[PPTNode, Option[PPTNode]], transformed: mutable.Map[PPTNode, PPTNode]): Unit = {
-    parents += node -> parent
-    transformed += node -> node
-    node.children.foreach(x => buildTopDown(x, Some(node), parents, transformed))
-  }
-
-  def add(rule: (PPTNode, PPTNodeContext) => Option[PPTNode]) = rules.insert(0, rule)
 }
 
-class PhysicalPlanOptimizerImpl extends AbstractPhysicalPlanOptimizer {
-  add(removeNullProject)
+object RemoveNullProject extends PhysicalPlanOptimizerRule {
+  override def apply(plan: PPTNode): PPTNode = optimizeBottomUp(plan,
+    {
+      case pnode: PPTNode =>
+        pnode.children match {
+          case Seq(p@PPTProject(ri)) if ri.items.forall {
+            case AliasedReturnItem(expression, variable) => expression == variable
+          } => pnode.withChildren(pnode.children.filterNot(_ eq p) ++ p.children)
 
-  def removeNullProject(node: PPTNode, ctx: PPTNodeContext) = node match {
-    case thisNode@PPTProject(ri: ReturnItemsDef) =>
-      if (ri.items.forall {
-        case AliasedReturnItem(expression, variable) => expression == variable
-        case _ => false
-      }) {
-        ctx.downStream.map(pnode => {
-          val newNode = pnode.withChildren(pnode.children.filterNot(_ == thisNode) ++ thisNode.children)
-          ctx.replaceWith(pnode, newNode)
-          newNode
+          case _ => pnode
         }
-        )
-      }
-      else
-        Some(thisNode)
-
-    case _ =>
-      Some(node)
-  }
+    }
+  )
 }
