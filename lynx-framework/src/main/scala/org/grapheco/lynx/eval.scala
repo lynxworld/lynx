a232c73f0da6b8272d7753a119edf7a28a5a7499
codeBabyLin <1163718576@qq.com>
2021-03-16 18:10:46 +0800

    aggre

--- lynx-framework/src/main/scala/org/grapheco/lynx/eval.scala
+++ lynx-framework/src/main/scala/org/grapheco/lynx/eval.scala
@@ -1,7 +1,76 @@
 package org.grapheco.lynx
 
 import org.opencypher.v9_0.expressions._
-import org.opencypher.v9_0.util.symbols.{CTAny, CTBoolean, CTFloat, CTInteger, CTString}
+import org.opencypher.v9_0.expressions.functions.Count
+import org.opencypher.v9_0.util.symbols.{CTAny, CTBoolean, CTFloat, CTInteger, CTString, CypherType}
+
+
+//codebaby add CTType of aggreFunction
+trait AggregationType{
+
+}
+
+sealed abstract class CountStarType extends CypherType with AggregationType
+object CountStarType {
+  val instance = new CountStarType() {
+    val parentType = CTAny
+    override val toString = "CountStar"
+    override val toNeoTypeString = "CountStar()"
+  }
+}
+sealed abstract class CountType extends CypherType with AggregationType
+object CountType {
+  val instance = new CountType() {
+    val parentType = CTAny
+    override val toString = "Count"
+    override val toNeoTypeString = "Count()"
+  }
+}
+
+sealed abstract class SumType extends CypherType with AggregationType
+object SumType {
+  val instance = new SumType() {
+    val parentType = CTAny
+    override val toString = "Sum"
+    override val toNeoTypeString = "Sum()"
+  }
+}
+sealed abstract class MaxType extends CypherType with AggregationType
+object MaxType {
+  val instance = new MaxType() {
+    val parentType = CTAny
+    override val toString = "Max"
+    override val toNeoTypeString = "Max()"
+  }
+}
+
+sealed abstract class MinType extends CypherType with AggregationType
+object MinType {
+  val instance = new MinType() {
+    val parentType = CTAny
+    override val toString = "Min"
+    override val toNeoTypeString = "Min()"
+  }
+}
+sealed abstract class AvgType extends CypherType with AggregationType
+object AvgType {
+  val instance = new AvgType() {
+    val parentType = CTAny
+    override val toString = "Avg"
+    override val toNeoTypeString = "Avg()"
+  }
+}
+
+object ExpandType{
+  val CTCountStar: CountStarType = CountStarType.instance
+  val CTCount: CountType = CountType.instance
+  val CTSum: SumType = SumType.instance
+  val CTMax: MaxType = MaxType.instance
+  val CTMin: MinType = MinType.instance
+  val CTAvg: AvgType = AvgType.instance
+}
+//codebaby add CTType of aggreFunction
+
 
 trait ExpressionEvaluator {
   def eval(expr: Expression)(implicit ec: ExpressionContext): LynxValue
@@ -38,6 +107,15 @@ class ExpressionEvaluatorImpl(graphModel: GraphModel) extends ExpressionEvaluato
       case _: StringLiteral => CTString
       case _: IntegerLiteral => CTInteger
       case _: DoubleLiteral => CTFloat
+      case CountStar() => ExpandType.CTCountStar
+      case FunctionInvocation(namespace, functionName, distinct, args) => functionName.name.toLowerCase match{
+        case "count" => ExpandType.CTCount
+        case "sum" => ExpandType.CTSum
+        case "max" => ExpandType.CTMax
+        case "min" => ExpandType.CTMin
+        case "avg" => ExpandType.CTAvg
+      }
+
 
       case Variable(name) => definedVarTypes(name)
       case _ => CTAny
@@ -72,7 +150,7 @@ class ExpressionEvaluatorImpl(graphModel: GraphModel) extends ExpressionEvaluato
         }
 
 
-      case pe:PathExpression => {
+      case pe: PathExpression => {
 
         evalStep(pe.step)
         //LynxList(pe.step.dependencies.map(eval).toList)
@@ -83,7 +161,17 @@ class ExpressionEvaluatorImpl(graphModel: GraphModel) extends ExpressionEvaluato
           case s: SingleRelationshipPathStep => LynxList(s.dependencies.map(eval).toList)
         }*/
       }
-      case f:FunctionInvocation => LynxFunction.getValue(f.namespace.parts, f.functionName.name, f.args.map(eval), graphModel)
+      case f: FunctionInvocation =>
+        f.functionName.name.toLowerCase match {
+          case "sum" => eval(f.args.head)
+          case "max" => eval(f.args.head)
+          case "min" => eval(f.args.head)
+          case "count" => LynxInteger(1)
+          case _ =>LynxFunction.getValue(f.namespace.parts, f.functionName.name, f.args.map(eval), graphModel)
+        }
+
+
+      case CountStar() => LynxInteger(ec.vars.size)
 
 
       case Add(lhs, rhs) =>
@@ -154,8 +242,8 @@ class ExpressionEvaluatorImpl(graphModel: GraphModel) extends ExpressionEvaluato
       case Property(src, PropertyKeyName(name)) =>
         eval(src) match {
           case LynxNull => LynxNull
-          case cn: LynxNode => cn.property(name).get
-          case cr: LynxRelationship => cr.property(name).get
+          case cn: LynxNode => cn.property(name).getOrElse(LynxNull)
+          case cr: LynxRelationship => cr.property(name).getOrElse(LynxNull)
         }
 
       case Parameter(name, parameterType) =>
