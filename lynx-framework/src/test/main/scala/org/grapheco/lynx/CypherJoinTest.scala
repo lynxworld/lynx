22eb6b3ec7af303a7ad0cd414b6959c184616973
excelwang <excelwang@users.noreply.github.com>
2021-05-26 16:17:25 +0800

    fix disordered return when join

--- lynx-framework/src/test/main/scala/org/grapheco/lynx/CypherJoinTest.scala
+++ lynx-framework/src/test/main/scala/org/grapheco/lynx/CypherJoinTest.scala
@@ -11,7 +11,15 @@ class CypherJoinTest extends TestBase {
     (c) where c.name = 'Bob'
     CREATE (a)-[r]->(b)
     RETURN a,b,c, r""".stripMargin
-    var rs = runOnDemoGraph(q).records().toSeq
-    println(rs)
+    val rs = runOnDemoGraph(q).records().toSeq.head
+    val a = rs.get("a").get.asInstanceOf[LynxNode]
+    val b = rs.get("b").get.asInstanceOf[LynxNode]
+    val c = rs.get("c").get.asInstanceOf[LynxNode]
+    val r = rs.get("r").get.asInstanceOf[LynxRelationship]
+    Assert.assertEquals("Alice", a.property("name").get.value)
+    Assert.assertEquals("bluejoe", b.property("name").get.value)
+    Assert.assertEquals("Bob", c.property("name").get.value)
+    Assert.assertEquals(a.id, r.startNodeId)
+    Assert.assertEquals(b.id, r.endNodeId)
   }
 }
