25e7c205461715641cd60dc6e6d9a2464780efca
airzihao <airzihao@163.com>
2021-06-17 14:03:10 +0800

    Init Standard Tests.

---
+++ lynx-cypher-test/src/test/scala/PropertyID.scala
@@ -0,0 +1,10 @@
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 1:31 下午 2021/6/17
+ * @Modified By:
+ */
+object PropertyID extends Enumeration {
+  val NAME = Value(1, "name")
+
+}
