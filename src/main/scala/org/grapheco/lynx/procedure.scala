bd512f05933d1c512f0d0053d89e0dde1bd40869
Airzihao <32104495+Airzihao@users.noreply.github.com>
2022-04-20 12:07:02 +0800

    Rollback commits about TypeSystem. Respect to Bluejoe's work. (#34)

--- src/main/scala/org/grapheco/lynx/procedure.scala
+++ src/main/scala/org/grapheco/lynx/procedure.scala
@@ -61,7 +61,8 @@ class DefaultProcedureRegistry(types: TypeSystem, classes: Class[_]*) extends Pr
 
         //TODO: N-tuples
         val outputs = Seq("value" -> types.typeOf(met.getReturnType))
-        register(an.name(), inputs, outputs, (args) => types.wrap(met.invoke(host, args: _*)))
+        register(an.name(), inputs, outputs, (args) =>
+          types.wrap(met.invoke(host, args.map(types.unwrap(_).asInstanceOf[Object]): _*)))
       }
     })
   }
