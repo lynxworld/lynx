c02e91ae793258f28ab008a067ea17ae8d7650db
Airzihao <airzihao@163.com>
2022-07-10 21:22:59 +0800

    Impl LynxNode.compareTo

--- src/main/scala/org/grapheco/lynx/types/structural/LynxNode.scala
+++ src/main/scala/org/grapheco/lynx/types/structural/LynxNode.scala
@@ -1,6 +1,6 @@
 package org.grapheco.lynx.types.structural
 
-import org.grapheco.lynx.types.LynxValue
+import org.grapheco.lynx.types.{LynxValue, TypeMismatchException}
 import org.grapheco.lynx.types.property.LynxNull
 import org.opencypher.v9_0.util.symbols.{CTNode, NodeType}
 
@@ -12,4 +12,9 @@ trait LynxNode extends LynxValue with HasProperty {
   def labels: Seq[LynxNodeLabel]
 
   def lynxType: NodeType = CTNode
+
+  override def compareTo(o: LynxValue): Int = o match {
+    case node: LynxNode => id.toLynxInteger.compareTo(node.id.toLynxInteger)
+    case _ => throw TypeMismatchException(this.lynxType, o.lynxType)
+  }
 }
