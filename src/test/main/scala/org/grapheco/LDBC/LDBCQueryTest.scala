2f6bf78acfc609a7371f0851e6b0dad919918488
胡川 <2585814664@qq.com>
2022-07-12 21:07:10 +0800

    change test params

--- src/test/main/scala/org/grapheco/LDBC/LDBCQueryTest.scala
+++ src/test/main/scala/org/grapheco/LDBC/LDBCQueryTest.scala
@@ -1,12 +1,14 @@
 package org.grapheco.LDBC
 
-import org.grapheco.LDBC.LDBCQueryTest.ldbcTestBase
+import org.grapheco.LDBC.LDBCQueryTest.{ldbcTestBase, personIds}
 import org.grapheco.lynx.types.property.LynxInteger
+import org.grapheco.lynx.types.structural.LynxPropertyKey
 import org.grapheco.lynx.types.time.LynxDate
 import org.grapheco.lynx.util.Profiler
 import org.junit.{Assert, BeforeClass, Test}
 
 import java.io.File
+import java.time.LocalDate
 import scala.io.Source
 
 /**
@@ -49,6 +51,13 @@ class LDBCQueryTest {
       case _ => Assert.assertTrue(false)
     }
   }
+
+  val update_person_ids = List("219791209300010", "226388279066632", "226388279066641", "210995116277761", "200000000000014", "200000000000016", "204398046511148", "215393162788899", "226388279066650", "221990232555526", "221990232555527", "200000000000027", "215393162788910", "210995116277782", "215393162788912", "200000000000033", "210995116277783", "226388279066664", "232985348833291", "200000000000047", "228587302322180", "202199023255557", "232985348833319", "228587302322191", "228587302322196")
+  val update_post_id = List("101030792151040", "101030792151041", "101030792151042", "101030792151043", "101030792151044", "101030792151045", "101030792151046", "101030792151047", "101030792151048", "101030792151049", "101030792151050", "101030792151051", "101030792151052", "101030792151053", "101030792151054", "101030792151055", "101030792151056", "101030792151057", "101030792151058", "100962072674323", "100962072674324", "100962072674325", "100962072674326", "100962072674327", "100962072674328")
+  val update_comment_id = List("401030792151558", "401030792151559", "401030792151560", "401030792151561", "401030792151562", "401030792151563", "401030792151564", "401030792151565", "401030792151566", "401030792151567", "401030792151568", "401030792151569", "401030792151570", "401030792151571", "401030792151572", "401030792151573", "401030792151575", "401030792151576", "401030792151577", "401030792151578", "401030792151579", "401030792151580", "401030792151581", "401030792151582", "401030792151583")
+  val update_forum_id = List("300687194767360", "300962072674305", "301030792151042", "300962072674307", "300687194767364", "300824633720838", "300824633720840", "300893353197579", "300962072674316", "300893353197581", "300893353197583", "300068719476752", "300481036337170", "300000000000019", "300206158430228", "300343597383701", "300412316860438", "300549755813911", "300687194767384", "300343597383706", "300481036337179", "300824633720860", "300618475290653", "300068719476766", "300137438953503")
+
+
 
   @Test
   def IS1(): Unit ={
@@ -102,7 +111,7 @@ class LDBCQueryTest {
   @Test
   def Q1(): Unit = {
     val q = getQuery("interactive-complex-1.cypher")
-    val p = Map("personId" -> "210995116277782", "firstName" -> "Ali")
+    val p = Map("personId" -> update_person_ids(0), "firstName"-> "Jose")
     run(q,p)
     //shortestPath
   }
@@ -110,14 +119,18 @@ class LDBCQueryTest {
   @Test
   def Q2(): Unit = {
     val q = getQuery("interactive-complex-2.cypher")
-    val p = Map("personId" -> "210995116277782", "maxDate" -> "1287230400000")
+    val p = Map("personId" -> update_person_ids(0), "maxDate" -> LynxDate.today)
     run(q,p)
   }
 //
   @Test
   def Q3(): Unit = {
     val q = getQuery("interactive-complex-3.cypher")
-    val p = Map("personId" -> "10995116277794", "countryXName" -> "Angola", "countryYName" -> "Colombia", "startDate" -> "1275393600000", "endDate" -> "1277812800000")
+    val p = Map("personId" -> update_person_ids(0),
+      "countryXName" -> "Angola",
+      "countryYName" -> "Colombia",
+      "startDate" -> LynxDate(LocalDate.of(1800,1,1)),
+      "endDate" -> LynxDate.today)
     run(q,p)
   }
 
@@ -125,56 +138,60 @@ class LDBCQueryTest {
   @Test
   def Q4(): Unit = {
     val q = getQuery("interactive-complex-4.cypher")
-    val p = Map("personId" -> "10995116277794", "startDate" -> "1275393600000", "endDate" -> "1277812800000")
+    val p = Map("personId" -> update_person_ids(0),
+      "startDate" -> LynxDate(LocalDate.of(1800,1,1)),
+      "endDate" -> LynxDate.today)
     run(q,p)
   }
 
   @Test
   def Q5(): Unit = {
     val q = getQuery("interactive-complex-5.cypher")
-    val p = Map("personId" -> "10995116277794", "minDate" -> "1287230400000")
+    val p = Map("personId" -> update_person_ids(0), "minDate" -> LynxDate(LocalDate.of(1800,1,1)))
     run(q,p)
   }
 
   @Test
   def Q6(): Unit = {
     val q = getQuery("interactive-complex-6.cypher")
-    val p = Map("personId" -> "10995116277794", "tagName" -> "Carl_Gustaf_Emil_Mannerheim")
+    val p = Map("personId" -> update_person_ids(0), "tagName" -> "Carl_Gustaf_Emil_Mannerheim")
     run(q,p)
   }
 
   @Test
   def Q7(): Unit = {
     val q = getQuery("interactive-complex-7.cypher")
-    val p = Map("personId" -> "10995116277794")
+    val p = Map("personId" -> update_person_ids(0))
     run(q,p)
   }
 
   @Test
   def Q8(): Unit = {
     val q = getQuery("interactive-complex-8.cypher")
-    val p = Map("personId" -> "10995116277794")
+    val p = Map("personId" -> update_person_ids(0))
     run(q,p)
   }
 
   @Test
   def Q9(): Unit = {
     val q = getQuery("interactive-complex-9.cypher")
-    val p = Map("personId" -> "10995116277794")
+    val p = Map("personId" -> update_person_ids(0), "maxDate" -> LynxDate.today)
     run(q,p)
   }
 
   @Test
   def Q10(): Unit = {
     val q = getQuery("interactive-complex-10.cypher")
-    val p = Map("personId" -> "10995116277794")
+    val p = Map("personId" -> update_person_ids(0), "month" -> 5)
     run(q,p)
   }
 
   @Test
   def Q11(): Unit = {
     val q = getQuery("interactive-complex-11.cypher")
-    val p = Map("personId" -> "10995116277794", "countryName" -> "Hungary", "workFromYear" -> "2011")
+    val p = Map("personId" -> update_person_ids(0),
+      "countryName" -> "Hungary",
+      "workFromYear" -> "2011")
     run(q,p)
   }
 
@@ -182,7 +199,7 @@ class LDBCQueryTest {
   @Test
   def Q12(): Unit = {
     val q = getQuery("interactive-complex-12.cypher")
-    val p = Map("personId" -> "10995116277794", "tagName" -> "Carl_Gustaf_Emil_Mannerheim")
+    val p = Map("personId" -> update_person_ids(0), "tagClassName" -> "Carl_Gustaf_Emil_Mannerheim")
     run(q,p)
   }
 
@@ -190,7 +207,7 @@ class LDBCQueryTest {
   @Test
   def Q13(): Unit = {
     val q = getQuery("interactive-complex-13.cypher")
-    val p = Map("person1Id" -> "10995116277794", "person2Id" -> "8796093022357")
+    val p = Map("person1Id" -> update_person_ids(0), "person2Id" -> update_person_ids(2))
     run(q,p)
   }
 
@@ -198,7 +215,7 @@ class LDBCQueryTest {
   @Test
   def Q14(): Unit = {
     val q = getQuery("interactive-complex-14.cypher")
-    val p = Map("person1Id" -> "10995116277794", "person2Id" -> "8796093022357")
+    val p = Map("person1Id" -> update_person_ids(0), "person2Id" -> update_person_ids(2))
     run(q,p)
   }
 
@@ -209,9 +226,6 @@ class LDBCQueryTest {
     run(q, p)
   }
 
-  val update_person_ids = List("219791209300010", "226388279066632", "226388279066641", "210995116277761", "200000000000014", "200000000000016", "204398046511148", "215393162788899", "226388279066650", "221990232555526", "221990232555527", "200000000000027", "215393162788910", "210995116277782", "215393162788912", "200000000000033", "210995116277783", "226388279066664", "232985348833291", "200000000000047", "228587302322180", "202199023255557", "232985348833319", "228587302322191", "228587302322196")
-  val update_post_id = List("101030792151040", "101030792151041", "101030792151042", "101030792151043", "101030792151044", "101030792151045", "101030792151046", "101030792151047", "101030792151048", "101030792151049", "101030792151050", "101030792151051", "101030792151052", "101030792151053", "101030792151054", "101030792151055", "101030792151056", "101030792151057", "101030792151058", "100962072674323", "100962072674324", "100962072674325", "100962072674326", "100962072674327", "100962072674328")
-
 
   @Test
   def u2(): Unit ={
@@ -220,9 +234,66 @@ class LDBCQueryTest {
       "postId" -> update_post_id(0),
       "creationDate" -> LynxDate.today)
     run(q, p)
-    val verify = "MATCH (person:Person {id: $personId})-[r:LIKES]-(post:Post {id: $postId}) return r"
-    val result = ldbcTestBase.run(verify, p)
-    result.records()
+    val verify = "MATCH (person:Person {id: $personId})-[r:LIKES]->(post:Post {id: $postId}) return r"
+    val result = ldbcTestBase.runner.run(verify, p)
+    Assert.assertTrue(
+      result.records()
+      .flatMap(_.getAsRelationship("r"))
+      .flatMap(_.property(LynxPropertyKey("creationDate")))
+      .map(_.asInstanceOf[LynxDate]).exists(LynxDate.today.equals))
   }
 
+  @Test
+  def u3(): Unit ={
+    val q = getQuery("interactive-update-3.cypher")
+    val p = Map("personId" -> update_person_ids(1),
+      "commentId" -> update_comment_id(0),
+      "creationDate" -> LynxDate.today)
+    run(q, p)
+    val verify = "MATCH (person:Person {id: $personId})-[r:LIKES]->(comment:Comment {id: $commentId}) return r"
+    val result = ldbcTestBase.runner.run(verify, p)
+    Assert.assertTrue(
+      result.records()
+        .flatMap(_.getAsRelationship("r"))
+        .flatMap(_.property(LynxPropertyKey("creationDate")))
+        .map(_.asInstanceOf[LynxDate]).exists(LynxDate.today.equals))
+  }
+
+  @Test
+  def u4(): Unit ={
+
+  }
+
+  @Test
+  def u5(): Unit ={
+    val q = getQuery("interactive-update-5.cypher")
+    val p = Map("personId" -> update_person_ids(2),
+      "forumId" -> update_forum_id(0),
+      "joinDate" -> LynxDate.today)
+    run(q, p)
+    val verify = "MATCH (f:Forum {id: $forumId})-[r:HAS_MEMBER]->(p:Person {id: $personId}) return r"
+    val result = ldbcTestBase.runner.run(verify, p)
+    Assert.assertTrue(
+      result.records()
+        .flatMap(_.getAsRelationship("r"))
+        .flatMap(_.property(LynxPropertyKey("joinDate")))
+        .map(_.asInstanceOf[LynxDate]).exists(LynxDate.today.equals))
+  }
+
+  @Test
+  def u8(): Unit ={
+    val q = getQuery(  "interactive-update-8.cypher")
+    val p = Map(
+      "person1Id" -> update_person_ids(8),
+      "person2Id" -> update_person_ids(9),
+      "creationDate" -> LynxDate.today)
+    run(q, p)
+    val verify = "MATCH (p1:Person {id: $person1Id})-[r:KNOWS]->(p2:Person {id: $person2Id}) return r"
+    val result = ldbcTestBase.runner.run(verify, p)
+    Assert.assertTrue(
+      result.records()
+        .flatMap(_.getAsRelationship("r"))
+        .flatMap(_.property(LynxPropertyKey("creationDate")))
+        .map(_.asInstanceOf[LynxDate]).exists(LynxDate.today.equals))
+  }
 }
