55b9be7e8de504ea2c798d38742abb1dcee203c1
Airzihao <airzihao@163.com>
2022-07-14 14:17:31 +0800

    Add EvaluatorException.

---
+++ src/main/scala/org/grapheco/lynx/evaluator/EvaluatorException.scala
@@ -0,0 +1,17 @@
+package org.grapheco.lynx.evaluator
+
+import org.grapheco.lynx.LynxException
+
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 10:15 2022/7/14
+ * @Modified By:
+ */
+case class EvaluatorException(msg: String) extends LynxException {
+  override def getMessage: String = msg
+}
+
+case class EvaluatorTypeMismatch(actualType: String, expectedType: String) extends LynxException {
+  override def getMessage: String = s"Type Mismatch, expected ${expectedType}, actual ${actualType}"
+}
