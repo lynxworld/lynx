5247293daf42f13c061303a3e4ab5ab87b5c186a
Dogknn <111851913+Dogknn@users.noreply.github.com>
2022-10-21 19:23:49 +0800

    Time&DateFunction (#53)
    * Predicate functions and Scalar functions
    
    * Predicate functions  Scalar functions Aggregating functions
    
    * commit List functions and Mathematical functions - numeric
    add Scalar functions and Aggregating functions block annotation
    
    * updata A Function to E Function &
    add Mathematical functions - logarithmic; Mathematical functions - trigonometric; String functions
    
    * commit Temporal functions - instant types; Temporal functions - duration; Spatial functions; User-defined functions
    
    * add Temporal functions-date(),
    
    * add Temporal functions-date(),add Temporal functions-date(),
    
    * 1.implement Temporal functions-until dateTimeFromTimestamp_1 in I_Temporal_InstantTypes.scala
    2.modify function relate to array in D_List.scala
    
    * rewrite Time&DateFunction
    
    * rewrite Time&DateFunction 10/21
    
    * log4j.properties

---
+++ src/main/scala/org/grapheco/lynx/types/time/LynxComponentDate.scala
@@ -0,0 +1,277 @@
+package org.grapheco.lynx.types.time
+
+import org.grapheco.lynx.types.composite.LynxMap
+import org.grapheco.lynx.types.property.{LynxInteger, LynxString}
+import org.grapheco.lynx.util.LynxTemporalParser
+import org.grapheco.lynx.util.LynxTemporalParser.{assureBetween, assureContains}
+
+import scala.util.matching.Regex
+
+import java.time.{LocalDateTime, ZonedDateTime}
+//import org.scalacheck.Prop.{False, True}
+
+import java.time.LocalDate
+import java.util.{Calendar, GregorianCalendar}
+
+trait LynxComponentDate {
+
+  //Integer at least 4 digits
+  var year: Int
+  //Integer 1 to 4
+  var quarter: Int
+  //Integer 1 to 12
+  var month: Int
+  //Integer 1 to 53
+  var week: Int
+  //Integer at least 4 digits
+  var weekYear: Int
+  //Integer 1 to 92
+  var dayOfQuarter: Int
+  //Integer 1 to 92
+  var quarterDay: Int
+  //Integer 1 to 31
+  var day: Int
+  //Integer 1 to 366
+  var ordinalDay: Int
+  //Integer 1 to 7
+  var dayOfWeek: Int
+  //Integer 1 to 7
+  var weekDay: Int
+
+}
+
+object LynxComponentDate {
+
+  def getYearMonthDay(map: Map[String, Any]): (Int, Int, Int) = {
+    val year: Int = map.get("year").map(
+      _ match {
+        case v: LynxInteger => v.value.toInt
+        case v: Long => v.toInt
+        case v: Int => v
+      }
+    ).getOrElse(0)
+
+    if (map.contains("month")) {
+      assureContains(map, "year")
+    }
+    val month: Int = map.get("month").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(1)
+
+    if (map.contains("day")) {
+      assureContains(map, "month")
+    }
+    val day: Int = map.get("day").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(1)
+
+    assureBetween(month, 1, 12, "month")
+    assureBetween(day, 1, 31, "day")
+    (year, month, day)
+  }
+
+  def transformYearWeekDay(map: Map[String, Any]): (Int, Int, Int) = {
+
+    val year: Int = map.get("year").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(0)
+
+    if (map.contains("week")) {
+      assureContains(map, "year")
+    }
+    val week: Int = map.get("week").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(1)
+
+    if (map.contains("dayOfWeek")) {
+      assureContains(map, "week")
+    }
+    val day: Int = map.get("dayOfWeek").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(1)
+
+    assureBetween(week, 1, 53, "week")
+    assureBetween(day, 1, 7, "dayOfWeek")
+    val calendar = new GregorianCalendar()
+    calendar.clear()
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.WEEK_OF_YEAR, week)
+    calendar.set(Calendar.DAY_OF_WEEK, day)
+    (calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH) + 1)
+  }
+
+  def transformYearQuarterDay(map: Map[String, Any]): (Int, Int, Int) = {
+
+    val year: Int = map.get("year").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(0)
+
+    if (map.contains("quarter")) {
+      assureContains(map, "year")
+    }
+    val quarter: Int = map.get("quarter").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(1)
+
+    if (map.contains("dayOfQuarter")) {
+      assureContains(map, "quarter")
+    }
+    val day: Int = map.get("dayOfQuarter").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(1)
+
+    assureBetween(quarter, 1, 4, "quarter")
+    assureBetween(day, 1, 92, "dayOfQuarter")
+
+    val calendar = new GregorianCalendar()
+    calendar.clear()
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.MONTH, quarter * 3 - 3)
+    calendar.set(Calendar.DAY_OF_MONTH, day)
+
+    (calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH))
+  }
+
+  def transformYearOrdinalDay(map: Map[String, Any]): (Int, Int, Int) = {
+
+    val year: Int = map.get("year").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(0)
+
+    if (map.contains("ordinalDay")) {
+      assureContains(map, "year")
+    }
+    val ordinalDay: Int = map.get("ordinalDay").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+      case v: Int => v
+    }).getOrElse(1)
+
+    assureBetween(ordinalDay, 1, 366, "ordinalDay")
+    val calendar = new GregorianCalendar()
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.DAY_OF_YEAR, ordinalDay)
+    (calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH))
+  }
+
+  def transformDate(map: Map[String, Any]): (Int, Int, Int) = {
+
+    var (year, month, day) = map.get("date").getOrElse(0) match {
+      case v: LocalDate => (v.getYear, v.getMonthValue, v.getDayOfMonth)
+      case v: ZonedDateTime => (v.getYear, v.getMonthValue, v.getDayOfMonth)
+      case v: LocalDateTime => (v.getYear, v.getMonthValue, v.getDayOfMonth)
+    }
+    if (map.contains("day")) {
+      assureContains(map, "date")
+    }
+    day = map.get("day").map(_ match {
+      case v: LynxInteger => v.value.toInt
+      case v: Long => v.toInt
+    }).getOrElse(day)
+
+    (year, month, day)
+  }
+
+  def getYearMonthDay(dateStr: String): (Int, Int, Int) = {
+    val YYYY_MM_DD: Regex = "^([0-9]{4})-([0-9]{2})-([0-9]{2})$".r
+    val YYYYMMDD: Regex = "^([0-9]{4})([0-9]{2})([0-9]{2})$".r
+    val YYYY_MM: Regex = "^([0-9]{4})-([0-9]{2})$".r
+    val YYYYMM: Regex = "^([0-9]{4})([0-9]{2})$".r
+    val YYYY_Www_D: Regex = "^([0-9]{4})-W([0-9]{2})-([1-7]{1})$".r
+    val YYYYWwwD: Regex = "^([0-9]{4})W([0-9]{2})([1-7]{1})$".r
+    val YYYY_Www: Regex = "^([0-9]{4})-W([0-9]{2})$".r
+    val YYYYWww: Regex = "^([0-9]{4})W([0-9]{2})$".r
+    val YYYY_Qq_DD: Regex = "^([0-9]{4})-Q([0-9]{1})-([0-9]{2})$".r
+    val YYYYQqDD: Regex = "^([0-9]{4})Q([0-9]{1})([0-9]{2})$".r
+    val YYYY_Qq: Regex = "^([0-9]{4})-Q([0-9]{1})$".r
+    val YYYYQq: Regex = "^([0-9]{4})Q([0-9]{1})$".r
+    val YYYY_DDD: Regex = "^([0-9]{4})-([0-9]{3})$".r
+    val YYYYDDD: Regex = "^([0-9]{4})([0-9]{3})$".r
+    val YYYY: Regex = "^([0-9]{4})$".r
+
+    dateStr match {
+      case YYYY_MM_DD(year, month, day) => (year.toInt, month.toInt, day.toInt)
+      case YYYYMMDD(year, month, day) => (year.toInt, month.toInt, day.toInt)
+      case YYYY_MM(year, month) => (year.toInt, month.toInt, 1)
+      case YYYYMM(year, month) => (year.toInt, month.toInt, 1)
+      case YYYY_Www_D(year, week, day) => transformYearWeekDay(Map("year" -> year.toInt, "week" -> week.toInt, "dayOfWeek" -> day.toInt))
+      case YYYYWwwD(year, week, day) => transformYearWeekDay(Map("year" -> year.toInt, "week" -> week.toInt, "dayOfWeek" -> day.toInt))
+      case YYYY_Www(year, week) => transformYearWeekDay(Map("year" -> year.toInt, "week" -> week.toInt, "dayOfWeek" -> 1))
+      case YYYYWww(year, week) => transformYearWeekDay(Map("year" -> year.toInt, "week" -> week.toInt, "dayOfWeek" -> 1))
+      case YYYY_Qq_DD(year, quarter, day) => transformYearQuarterDay(Map("year" -> year.toInt, "quarter" -> quarter.toInt, "dayOfQuarter" -> day.toInt))
+      case YYYYQqDD(year, quarter, day) => transformYearQuarterDay(Map("year" -> year.toInt, "quarter" -> quarter.toInt, "dayOfQuarter" -> day.toInt))
+      case YYYY_Qq(year, quarter) => transformYearQuarterDay(Map("year" -> year.toInt, "quarter" -> quarter.toInt, "dayOfQuarter" -> 1))
+      case YYYYQq(year, quarter) => transformYearQuarterDay(Map("year" -> year.toInt, "quarter" -> quarter.toInt, "dayOfQuarter" -> 1))
+      case YYYY_DDD(year, ordinalDay) => transformYearOrdinalDay(Map("year" -> year.toInt, "ordinalDay" -> ordinalDay.toInt))
+      case YYYYDDD(year, ordinalDay) => transformYearOrdinalDay(Map("year" -> year.toInt, "ordinalDay" -> ordinalDay.toInt))
+      case YYYY(year) => (year.toInt, 1, 1)
+    }
+  }
+
+  def truncateDate(map: Map[String, Any]): (Int, Int, Int) = {
+
+    val calendar = Calendar.getInstance()
+    calendar.clear()
+
+    val date = map.get("dateValue").getOrElse(0).asInstanceOf[LynxDateTime].zonedDateTime
+
+    val (year, month, day): (Int, Int, Int) = (date.getYear, date.getMonthValue - 1, date.getDayOfMonth)
+    calendar.setFirstDayOfWeek(Calendar.MONDAY)
+    map.get("unitStr").get match {
+      case LynxString("millennium")
+      => calendar.set(year - year % 1000, 0, 1)
+      case LynxString("century")
+      => calendar.set(year - year % 100, 0, 1)
+      case LynxString("decade")
+      => calendar.set(year - year % 10, 0, 1)
+      case LynxString("year")
+      => calendar.set(year, 0, 1)
+      case LynxString("weekYear")
+      => calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.WEEK_OF_YEAR, year)
+      //        calendar.set(year, 0, 1)
+      //        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_WEEK) match {
+      //          case 1 => 2
+      //          case 2 => 1
+      //          case v => 10 - v.toInt
+      //        })
+      case LynxString("quarter")
+      => calendar.set(year, month - month % 3, 1)
+      case LynxString("month")
+      => calendar.set(year, month, 1)
+      case LynxString("week")
+      => calendar.set(year, month, 1)
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_WEEK) match {
+          case 1 => 2
+          case 2 => 1
+          case v => 10 - v.toInt
+        })
+      case LynxString("day")
+      => calendar.set(year, month, day)
+    }
+    val addDay = map.size match {
+      case 2 =>
+      case 3 => calendar.add(
+        Calendar.DAY_OF_YEAR, map.get("mapOfComponents").get.asInstanceOf[LynxMap].value.map(_._2).head.asInstanceOf[LynxInteger].value.toInt - 1)
+    }
+    (calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH))
+  }
+}
--- src/main/scala/org/grapheco/lynx/types/time/LynxDateTime.scala
+++ src/main/scala/org/grapheco/lynx/types/time/LynxDateTime.scala
@@ -1,9 +1,18 @@
 package org.grapheco.lynx.types.time
 
 import org.grapheco.lynx.types.LynxValue
+import org.grapheco.lynx.types.composite.LynxMap
+import org.grapheco.lynx.types.property.{LynxInteger, LynxString}
+import org.grapheco.lynx.types.time.LynxComponentDate.{getYearMonthDay, transformDate, transformYearOrdinalDay, transformYearQuarterDay, transformYearWeekDay}
+import org.grapheco.lynx.types.time.LynxComponentTime.{getHourMinuteSecond, getNanosecond}
+import org.grapheco.lynx.types.time.LynxComponentTimeZone.{getOffset, getZone}
+import org.grapheco.lynx.util.{LynxTemporalParseException, LynxTemporalParser}
+import org.grapheco.lynx.util.LynxTemporalParser.splitDateTime
 import org.opencypher.v9_0.util.symbols.{CTDateTime, DateTimeType}
 
-import java.time.ZonedDateTime
+import java.sql.Timestamp
+import java.time.{LocalDateTime, LocalTime, ZoneId, ZoneOffset, ZonedDateTime}
+import java.util.{Calendar, Date, GregorianCalendar}
 
 /**
  * @ClassName LynxDateTime
@@ -12,10 +21,156 @@ import java.time.ZonedDateTime
  * @Date 2022/4/1
  * @Version 0.1
  */
-case class LynxDateTime(zonedDateTime: ZonedDateTime) extends LynxTemporalValue {
+case class LynxDateTime(zonedDateTime: ZonedDateTime) extends LynxTemporalValue with LynxComponentDate with LynxComponentTime with LynxComponentTimeZone {
   def value: ZonedDateTime = zonedDateTime
 
   def lynxType: DateTimeType = CTDateTime
 
   override def sameTypeCompareTo(o: LynxValue): Int = ???
+
+
+  //LynxComponentDate
+  val calendar = new GregorianCalendar()
+  calendar.set(Calendar.YEAR, zonedDateTime.getYear)
+  calendar.set(Calendar.DAY_OF_YEAR, zonedDateTime.getDayOfYear)
+  var year: Int = zonedDateTime.getYear
+  var month: Int = zonedDateTime.getMonthValue
+  var quarter: Int = month - month % 3
+  var week: Int = calendar.getWeeksInWeekYear
+  var weekYear: Int = calendar.getWeekYear
+  var ordinalDay: Int = zonedDateTime.getDayOfYear
+  val calendar_quarterBegin = new GregorianCalendar()
+  calendar_quarterBegin.set(Calendar.YEAR, zonedDateTime.getYear)
+  calendar_quarterBegin.set(Calendar.MONTH, quarter)
+  calendar_quarterBegin.set(Calendar.DAY_OF_MONTH, 1)
+  var dayOfQuarter: Int = calendar.get(Calendar.DAY_OF_YEAR) - calendar_quarterBegin.get(Calendar.DAY_OF_YEAR) + 1
+  var quarterDay: Int = calendar.get(Calendar.DAY_OF_YEAR) - calendar_quarterBegin.get(Calendar.DAY_OF_YEAR) + 1
+  var day: Int = zonedDateTime.getDayOfMonth
+  var dayOfWeek: Int = calendar.get(Calendar.DAY_OF_WEEK)
+  var weekDay: Int = calendar.get(Calendar.DAY_OF_WEEK)
+
+  //LynxComponentTime
+  var hour: Int = zonedDateTime.getHour
+  var minute: Int = zonedDateTime.getMinute
+  var second: Int = zonedDateTime.getSecond
+  var millisecond: Int = (zonedDateTime.getNano * Math.pow(0.1, 6)).toInt
+  var microsecond: Int = (zonedDateTime.getNano * Math.pow(0.1, 3) - millisecond * Math.pow(10, 3)).toInt
+  var nanosecond: Int = zonedDateTime.getNano % Math.pow(10, 3).toInt
+  var fraction: Int = zonedDateTime.getNano
+
+  //LynxComponentZone
+  var timeZone: String = zonedDateTime.getOffset.getId
+  var offset: String = zonedDateTime.getOffset.toString
+  var offsetMinutes: Int = zonedDateTime.getOffset.getTotalSeconds / 60
+  var offsetSeconds: Int = zonedDateTime.getOffset.getTotalSeconds
+
+
+  var epochMillis: Long = zonedDateTime.toInstant.toEpochMilli
+  var epochSeconds: Long = zonedDateTime.toInstant.toEpochMilli * Math.pow(0.1, 3).toInt
+
+
+}
+
+
+object LynxDateTime extends LynxTemporalParser {
+
+  def now(): LynxDateTime = LynxDateTime(ZonedDateTime.now())
+
+  def now(zoneId: ZoneId): LynxDateTime = LynxDateTime(ZonedDateTime.now(zoneId))
+
+  def of(zonedDateTimeStr: String): LynxDateTime = {
+    try {
+      LynxDateTime.of(zonedDateTimeStr)
+    } catch {
+      case _ => throw new Exception("DateTimeOfException")
+    }
+  }
+
+  def of(zonedDateTime: ZonedDateTime): LynxDateTime = LynxDateTime(zonedDateTime)
+
+  def of(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int, nanosecond: Int, timezone: String): LynxDateTime =
+    LynxDateTime(ZonedDateTime.of(year, month, day, hour, minute, second, nanosecond, LynxComponentTimeZone.getZone(timezone)))
+
+
+  def parse(zonedDateTimeStr: String, zoneId: ZoneId): LynxDateTime = {
+    //    val map = splitDateTime(zonedDateTimeStr)
+    //    map.size match {
+    //      case 3 => of(getYearMonthDay(map.get("dateStr").get), getHourMinuteSecond(map.get("timeStr").get), zoneId.getId)
+    //      case 4 => of(getYearMonthDay(map.get("dateStr").get), getHourMinuteSecond(map.get("timeStr").get), zoneId.getId, map.get("utcStr").get)
+    //    }
+    try {
+      val v = ZonedDateTime.parse(zonedDateTimeStr).toLocalDateTime.atZone(zoneId)
+      LynxDateTime(v)
+    } catch {
+      case _ => throw new Exception("DateTimeParseException")
+    }
+  }
+
+  def parse(zonedDateTimeStr: String): LynxDateTime = {
+    val map = splitDateTime(zonedDateTimeStr)
+    val dateTuple = getYearMonthDay(map.get("dateStr").get)
+    val timeTuple = getHourMinuteSecond(map.get("timeStr").get)
+    val zoneStr = getZone(map.get("zoneStr").getOrElse(null))
+    val offsetStr = getOffset(map.get("offsetStr").getOrElse(null))
+
+    var dateStr = dateTuple._1.formatted("%04d") + "-" + dateTuple._2.formatted("%02d") + "-" + dateTuple._3.formatted("%02d")
+    var timeStr = timeTuple._1.formatted("%02d") + ":" + timeTuple._2.formatted("%02d") + ":" + timeTuple._3.formatted("%02d") + (timeTuple._4 match {
+      case 0 => ""
+      case v: Int => "." + v.toString
+    })
+    var dateTime = LocalDateTime.parse(dateStr + "T" + timeStr)
+    zoneStr match {
+      case null => LynxDateTime(dateTime.atOffset(ZoneOffset.of(offsetStr)).toZonedDateTime)
+      case v: ZoneId => LynxDateTime(dateTime.atZone(v))
+    }
+  }
+
+  def parse(map: Map[String, Any]): LynxDateTime = {
+    if (map.isEmpty) {
+      throw LynxTemporalParseException("At least one temporal unit must be specified")
+    }
+
+    val zoneId = getZone(map.getOrElse("timezone", "Z") match {
+      case v: String => v.replace(" ", "_")
+    })
+    if (map.size == 1) {
+      map match {
+        case m if m.contains("timezone") => LynxDateTime.now(zoneId)
+        case m if m.contains("datetime") => LynxDateTime.parse(m.get("datetime").get.toString)
+        case m if m.contains("epochSeconds") => LynxDateTime(ZonedDateTime.ofInstant(new Timestamp(LynxDateTime.now().epochMillis).toInstant(), zoneId))
+        case m if m.contains("epochMillis") => LynxDateTime(ZonedDateTime.ofInstant(new Timestamp(LynxDateTime.now().epochSeconds).toInstant(), zoneId))
+      }
+    } else {
+      var v: ZonedDateTime = null
+      val (year, month, day) = map match {
+        case m if m.contains("dayOfWeek") => transformYearWeekDay(m)
+        case m if m.contains("dayOfQuarter") => transformYearQuarterDay(m)
+        case m if m.contains("ordinalDay") => transformYearOrdinalDay(m)
+        case m if m.contains("date") => transformDate(m)
+        case _ => getYearMonthDay(map)
+      }
+
+      val (hour, minute, second) = map match {
+        case m if m.contains("time") =>
+          val v = m("time").asInstanceOf[LocalTime]
+          (v.getHour, v.getMinute,
+            if (m.contains("second")) m("second") match {
+              case v: Long => v.toInt
+              case v: LynxInteger => v.value.toInt
+            }
+            else v.getSecond
+          )
+        case _ => getHourMinuteSecond(map, false)
+      }
+      val nanoOfSecond = map match {
+        case m if m.contains("time") => m("time") match {
+          case v: LocalTime => v.getNano
+          case v: LynxInteger => v.value.toInt
+        }
+        case _ => getNanosecond(map, false)
+      }
+      of(year, month, day, hour, minute, second, nanoOfSecond, zoneId.getId)
+    }
+  }
+
 }
