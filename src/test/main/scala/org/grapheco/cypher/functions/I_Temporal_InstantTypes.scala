4a39182343ffcac637155d8a1b0c6b3860cc5f35
RaoJunjian <jjrao@cnic.cn>
2022-11-10 12:18:01 +0800

    Added dateTransaction function

--- src/test/main/scala/org/grapheco/cypher/functions/I_Temporal_InstantTypes.scala
+++ src/test/main/scala/org/grapheco/cypher/functions/I_Temporal_InstantTypes.scala
@@ -42,7 +42,19 @@ class I_Temporal_InstantTypes extends TestBase {
   }
 
   @Test
-  def dateTransaction(): Unit = {
+  def dateTransaction():Unit={
+    val records = runOnDemoGraph(
+      """
+        |RETURN date.transaction() AS currentDate
+        |""".stripMargin).records().toArray
+
+    val now_date = LynxDate.now
+    Assert.assertEquals(1, records.length)
+    Assert.assertEquals(now_date, records(0)("currentDate"))
+  }
+
+  @Test
+  def dateStatement(): Unit = {
     val records = runOnDemoGraph(
       """
         |RETURN date.statement() AS currentDate
