cc5d187713b6409b8408d570ef922f8d655d733e
胡川, Hu Chuan <2585814664@qq.com>
2022-12-26 15:15:50 +0800

    Implemented UNION (#103)
    * fix bug
    
    * impl UNION

--- src/main/scala/org/grapheco/lynx/physical/PPT.scala
+++ src/main/scala/org/grapheco/lynx/physical/PPT.scala
@@ -890,7 +890,22 @@ case class PPTRemove(removeItems: Seq[RemoveItem])(implicit val in: PPTNode, val
 }
 
 ////////////////////////////
+case class PPTUnion(distinct: Boolean)(a: PPTNode, b: PPTNode, val plannerContext: PhysicalPlannerContext) extends AbstractPPTNode {
+  override val children: Seq[PPTNode] = Seq(a,b)
 
+  override val schema: Seq[(String, LynxType)] = a.schema
+
+  override def execute(implicit ctx: ExecutionContext): DataFrame = {
+    val schema1 = a.schema
+    val schema2 = b.schema
+    if (!schema1.toSet.equals(schema2.toSet)) throw SyntaxErrorException("All sub queries in an UNION must have the same column names")
+    val record1 = a.execute(ctx).records
+    val record2 = b.execute(ctx).records
+    val df = DataFrame(schema, ()=> record1 ++ record2)
+    if (distinct) df.distinct() else df
+  }
+  override def withChildren(children0: Seq[PPTNode]): PPTNode = PPTUnion(distinct)(children0.head, children0(1), plannerContext)
+}
 /////////UNWIND//////////////
 case class PPTUnwindTranslator(expression: Expression, variable: Variable) extends PPTNodeTranslator {
   override def translate(in: Option[PPTNode])(implicit ppc: PhysicalPlannerContext): PPTNode = {
